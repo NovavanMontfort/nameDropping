40e17489c7defca69c147ea1baa35d974a4162a7
novavanmontfort <novamontfort@hotmail.com>
2025-12-02 09:18:26 +0100

    Add Matter.js simulation with responsive logos

--- nameDropping/src/main.js
+++ nameDropping/src/main.js
@@ -1,12 +1,54 @@
+// Kleine wrapper die checkt of JS en touch aanwezig zijn,
+// en voegt klassen toe aan de HTML tag voor eventueel CSS gebruik.
 !(function (o, c) {
   var n = c.documentElement,
     t = " w-mod-";
-  (n.className += t + "js"),
-    ("ontouchstart" in o || (o.DocumentTouch && c instanceof DocumentTouch)) &&
-      (n.className += t + "touch");
+  n.className += t + "js";
+  ("ontouchstart" in o || (o.DocumentTouch && c instanceof DocumentTouch)) &&
+    (n.className += t + "touch");
 })(window, document);
 
-function initSimulation() {
+
+// Load image, get real width/height, create body
+// --------------------------------------------------
+// Deze functie maakt een Matter.js body op basis van een echte afbeelding.
+// De afbeelding wordt eerst geladen zodat de width/height bekend zijn.
+function loadImageBody(x, y, imgSrc, radius = 20) {
+  return new Promise((resolve) => {
+    const img = new Image();
+    img.src = imgSrc;
+
+    img.onload = () => {
+      // Bepaal schaal op basis van schermbreedte
+      const scale = getScaleForScreen();
+
+      // Geschaalde afmetingen
+      const scaledW = img.width * scale;
+      const scaledH = img.height * scale;
+
+      // Body aanmaken met geschaalde breedte/hoogte
+      const body = Matter.Bodies.rectangle(x, y, scaledW, scaledH, {
+        chamfer: { radius },
+        render: {
+          sprite: {
+            texture: imgSrc,
+            xScale: scale,
+            yScale: scale
+          }
+        }
+      });
+
+      resolve(body);
+    };
+  });
+}
+
+
+
+// Start van de volledige Matter.js simulatie
+// --------------------------------------------------
+async function initSimulation() {
+  // Matter.js modules importeren
   var Engine = Matter.Engine,
     Render = Matter.Render,
     Events = Matter.Events,
@@ -15,265 +57,108 @@ function initSimulation() {
     World = Matter.World,
     Bodies = Matter.Bodies;
 
+  // Fysica engine en wereld maken
   var engine = Engine.create(),
     world = engine.world;
+
+  // Canvas container selecteren
   var containerElement = document.querySelector(".tag-canvas");
   var containerWidth = containerElement.clientWidth + 2;
   var containerHeight = containerElement.clientHeight + 2;
 
+  // Renderer (canvas) maken
   var render = Render.create({
     element: containerElement,
     engine: engine,
     options: {
       width: containerWidth,
       height: containerHeight,
-      pixelRatio: 2,
+      pixelRatio: 2,          // scherpe rendering op retina
       background: "transparent",
       borderColor: "transparent",
-      wireframes: false
+      wireframes: false       // echte textures tonen ipv outlines
     }
   });
 
-  var thickness = 50; // dikte muur
+  var thickness = 50; // dikte van de onzichtbare muren
 
-var ground = Bodies.rectangle(
-  containerWidth / 2,
-  containerHeight + thickness / 2,
-  containerWidth,
-  thickness,
-  { isStatic: true }
-);
-
-var roof = Bodies.rectangle(
-  containerWidth / 2,
-  -thickness / 2,
-  containerWidth,
-  thickness,
-  { isStatic: true }
-);
-
-var wallLeft = Bodies.rectangle(
-  -thickness / 2,
-  containerHeight / 2,
-  thickness,
-  containerHeight,
-  { isStatic: true }
-);
-
-var wallRight = Bodies.rectangle(
-  containerWidth + thickness / 2,
-  containerHeight / 2,
-  thickness,
-  containerHeight,
-  { isStatic: true }
-);
-
-
-  var border = 0;
-  var radius = 20;
-
-  var tagBim = Bodies.rectangle(containerWidth / 2 + 150, 500, 164, 56, {
-    chamfer: { radius: radius },
-    render: {
-      sprite: {
-        texture:
-          "/public/bim.png",
-        xScale: 1,
-        yScale: 1
-      }
-    }
-  });
-  
-  var tagCheckpointIC = Bodies.rectangle(containerWidth / 2 + 150, 460, 240, 56, {
-    chamfer: { radius: radius },
-    render: {
-      sprite: {
-        texture:
-          "/public/checkpointIC.png",
-        xScale: 1,
-        yScale: 1
-      }
-    }
-  });
-  var tagFairPlay = Bodies.rectangle(containerWidth / 2 + 250, 420, 200, 56, {
-    chamfer: { radius: radius },
-    render: {
-      sprite: {
-        texture:
-          "/public/fairPlay.png",
-        xScale: 1,
-        yScale: 1
-      }
-    }
-  });
-  var tagForumGroningen = Bodies.rectangle(containerWidth / 2 - 75, 380, 160, 56, {
-    chamfer: { radius: radius },
-    render: {
-      sprite: {
-        texture:
-          "/public/forumGroningen.png",
-        xScale: 1,
-        yScale: 1
-      }
-    }
-  });
-
-  var tagGekkoo = Bodies.rectangle(
-    containerWidth / 2 - 74,
-    540,
-    248,
-    56,
-    {
-      chamfer: { radius: radius },
-      render: {
-        sprite: {
-          texture:
-            "/public/gekkoo.png",
-          xScale: 1,
-          yScale: 1
-        }
-      }
-    }
+  // Vloer
+  var ground = Bodies.rectangle(
+    containerWidth / 2,
+    containerHeight + thickness / 2,
+    containerWidth,
+    thickness,
+    { isStatic: true }
   );
-  var tagLuxaFlex = Bodies.rectangle(containerWidth / 2 + 174, 490, 105, 56, {
-    chamfer: { radius: radius },
-    render: {
-      sprite: {
-        texture:
-          "/public/luxaFlex.png",
-        xScale: 1,
-        yScale: 1
-      }
-    }
-  });
-  var tagNapoleonCasino = Bodies.rectangle(containerWidth / 2 - 142, 440, 186, 56, {
-    chamfer: { radius: radius },
-    render: {
-      sprite: {
-        texture:
-          "/public/napoleonCasino.png",
-        xScale: 1,
-        yScale: 1
-      }
-    }
-  });
-  var tagNederlandsKamerkoor = Bodies.rectangle(containerWidth / 2 - 10, 260, 128, 56, {
-    chamfer: { radius: radius },
-    render: {
-      sprite: {
-        texture:
-          "/public/nederlandsKamerkoor.png",
-        xScale: 1,
-        yScale: 1
-      }
-    }
-  });
-  var tagNieuweVeste = Bodies.rectangle(containerWidth / 2 - 242, 420, 168, 56, {
-    chamfer: { radius: radius },
-    render: {
-      sprite: {
-        texture:
-          "/public/nieuweVeste.png",
-        xScale: 1,
-        yScale: 1
-      }
-    }
-  });
-  var tagOrkater = Bodies.rectangle(containerWidth / 2 + 60, 380, 155, 56, {
-    chamfer: { radius: radius },
-    render: {
-      sprite: {
-        texture:
-          "/public/orkater.png",
-        xScale: 1,
-        yScale: 1
-      }
-    }
-  });
-  var tagParadisoMelkweg = Bodies.rectangle(containerWidth / 2, 360, 180, 56, {
-    chamfer: { radius: radius },
-    render: {
-      sprite: {
-        texture:
-          "/public/paradisoMelkweg.png",
-        xScale: 1,
-        yScale: 1
-      }
-    }
-  });
-  var tagPinkpop = Bodies.rectangle(containerWidth / 2 - 59, 260, 172, 56, {
-    chamfer: { radius: radius },
-    render: {
-      sprite: {
-        texture:
-          "/public/pinkpop.png",
-        xScale: 1,
-        yScale: 1
-      }
-    }
-  });
-  var tagSchouwburgCTilburg = Bodies.rectangle(containerWidth / 2 - 59, 260, 115, 56, {
-    chamfer: { radius: radius },
-    render: {
-      sprite: {
-        texture:
-          "/public/schouwburgCTilburg.png",
-        xScale: 1,
-        yScale: 1
-      }
-    }
-  });
-  var tagVnab = Bodies.rectangle(containerWidth / 2 - 59, 260, 210, 56, {
-    chamfer: { radius: radius },
-    render: {
-      sprite: {
-        texture:
-          "/public/vnab.png",
-        xScale: 1,
-        yScale: 1
-      }
-    }
-  });
 
-  World.add(engine.world, [
-    ground,
-    wallLeft,
-    wallRight,
-    roof,
-    tagBim,
-    tagCheckpointIC,
-    tagFairPlay,
-    tagForumGroningen,
-    tagGekkoo,
-    tagLuxaFlex,
-    tagNapoleonCasino,
-    tagNederlandsKamerkoor,
-    tagNieuweVeste,
-    tagOrkater,
-    tagParadisoMelkweg,
-    tagPinkpop,
-    tagSchouwburgCTilburg,
-    tagVnab,
-  ]);
+  // Plafond
+  var roof = Bodies.rectangle(
+    containerWidth / 2,
+    -thickness / 2,
+    containerWidth,
+    thickness,
+    { isStatic: true }
+  );
 
+  // Linker muur
+  var wallLeft = Bodies.rectangle(
+    -thickness / 2,
+    containerHeight / 2,
+    thickness,
+    containerHeight,
+    { isStatic: true }
+  );
+
+  // Rechter muur
+  var wallRight = Bodies.rectangle(
+    containerWidth + thickness / 2,
+    containerHeight / 2,
+    thickness,
+    containerHeight,
+    { isStatic: true }
+  );
+
+  // Alle PNG-logo’s laden en omzetten naar echte Matter.js bodies
+const bodies = await Promise.all([
+  loadImageBody(containerWidth * 0.1, 500, "/public/bim.png"),
+  loadImageBody(containerWidth * 0.18, 460, "/public/checkpointIC.png"),
+  loadImageBody(containerWidth * 0.26, 420, "/public/fairPlay.png"),
+  loadImageBody(containerWidth * 0.34, 380, "/public/forumGroningen.png"),
+  loadImageBody(containerWidth * 0.42, 540, "/public/gekkoo.png"),
+  loadImageBody(containerWidth * 0.5, 490, "/public/luxaFlex.png"),
+  loadImageBody(containerWidth * 0.58, 440, "/public/napoleonCasino.png"),
+  loadImageBody(containerWidth * 0.66, 260, "/public/nederlandsKamerkoor.png"),
+  loadImageBody(containerWidth * 0.74, 420, "/public/nieuweVeste.png"),
+  loadImageBody(containerWidth * 0.82, 380, "/public/orkater.png"),
+  loadImageBody(containerWidth * 0.9, 400, "/public/paradisoMelkweg.png"),
+  loadImageBody(containerWidth * 0.26, 420, "/public/pinkpop.png"),
+  loadImageBody(containerWidth * 0.95, 260, "/public/schouwburgCTilburg.png"),
+  loadImageBody(containerWidth * 1.0, 260, "/public/vnab.png")
+]);
+
+
+
+  // Alle muren en logo bodies toevoegen aan de wereld
+  World.add(engine.world, [ground, wallLeft, wallRight, roof, ...bodies]);
+
+  // Mouse interactie activeren (slepen)
   var mouse = Mouse.create(render.canvas),
     mouseConstraint = MouseConstraint.create(engine, {
       mouse: mouse,
       constraint: {
         stiffness: 0.2,
-        render: {
-          visible: false
-        }
+        render: { visible: false } // onzichtbare grijplijn
       }
     });
 
   World.add(world, mouseConstraint);
-
   render.mouse = mouse;
 
+  // Scrollblokkades verwijderen (voorkomt rare zoom)
   mouse.element.removeEventListener("mousewheel", mouse.mousewheel);
   mouse.element.removeEventListener("DOMMouseScroll", mouse.mousewheel);
 
+  // Detecteren of iets een klik of drag was
   let click = false;
 
   document.addEventListener("mousedown", () => (click = true));
@@ -282,21 +167,24 @@ var wallRight = Bodies.rectangle(
     console.log(click ? "click" : "drag")
   );
 
+  // Klik-detectie voor hyperlinks op een body
   Events.on(mouseConstraint, "mouseup", function (event) {
     var mouseConstraint = event.source;
     var bodies = engine.world.bodies;
+
+    // Alleen uitvoeren als er niet gesleept wordt
     if (!mouseConstraint.bodyB) {
       for (let i = 0; i < bodies.length; i++) {
-      var body = bodies[i];
+        var body = bodies[i];
         if (click === true) {
+          // Check of muis binnen de bounds van de body was
           if (
             Matter.Bounds.contains(body.bounds, mouseConstraint.mouse.position)
           ) {
             var bodyUrl = body.url;
-            console.log("Body.Url >> " + bodyUrl);
+
             if (bodyUrl != undefined) {
-              window.open(bodyUrl, "_blank");
-              console.log("Hyperlink was opened");
+              window.open(bodyUrl, "_blank"); // open link
             }
             break;
           }
@@ -305,27 +193,42 @@ var wallRight = Bodies.rectangle(
     }
   });
 
-  Events.on(engine, "beforeUpdate", function() {
-  world.bodies.forEach(body => {
-    if (body.position.y < -100) {
-      Matter.Body.setVelocity(body, { x: 0, y: 0 });
-      Matter.Body.setPosition(body, { x: body.position.x, y: 0 });
-    }
+  // Reset van bodies die te hoog vliegen (failsafe)
+  Events.on(engine, "beforeUpdate", function () {
+    world.bodies.forEach((body) => {
+      if (body.position.y < -100) {
+        Matter.Body.setVelocity(body, { x: 0, y: 0 });
+        Matter.Body.setPosition(body, { x: body.position.x, y: 0 });
+      }
+    });
   });
-});
 
+  // Engine + renderer starten
   Engine.run(engine);
   Render.run(render);
 }
 
+function getScaleForScreen() {
+  const width = window.innerWidth;
+
+  if (width < 480) return 0.45;  // kleine telefoon
+  if (width < 768) return 0.6;   // grote telefoon / kleine tablets
+  if (width < 1024) return 0.8;  // tablets
+  return 1;                      // laptops / desktops
+}
+
+// Simulatie alleen starten wanneer het element in beeld komt
 var containerElement = document.querySelector(".tag-canvas");
-var observer = new IntersectionObserver((entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      initSimulation();
-      observer.disconnect();
-    }
-  });
-}, {});
+var observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        initSimulation(); // start het pas wanneer zichtbaar
+        observer.disconnect(); // één keer uitvoeren
+      }
+    });
+  },
+  {}
+);
 
 observer.observe(containerElement);
